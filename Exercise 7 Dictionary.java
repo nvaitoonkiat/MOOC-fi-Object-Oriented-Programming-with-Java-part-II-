e370a216d538f41b9b5285971c6986f6efa07c18
nvaitoonkiat <nvaitoonkiat@gmail.com>
2017-11-16 10:38:23 -0500

    Add files via upload

--- Exercise 7 Dictionary.java
+++ Exercise 7 Dictionary.java
@@ -2,6 +2,65 @@ import java.lang.*;
 import java.io.*;
 import java.util.*;
 
+class TextUserInterface {
+
+    private final Dictionary dict;
+    private final Scanner read;
+
+    public TextUserInterface(Scanner reader, Dictionary dictionary) {
+        this.dict = dictionary;
+        this.read = reader;
+    }
+
+    public void start() {
+
+        startStatements();
+
+        while (true) {
+
+            System.out.println("Statement:");
+
+            String input = read.nextLine().trim();
+
+            if (input.equalsIgnoreCase("add")) {
+                add();
+            } else if (input.equalsIgnoreCase("translate")) {
+                translate();
+            } else if (input.equalsIgnoreCase("quit")) {
+                break;
+            } else {
+                System.out.println("Unknown statement");
+            }
+        }
+    }
+
+    public void startStatements() {
+        System.out.println("Statements:");
+        System.out.println("add - adds a word pair to the dictionary");
+        System.out.println("translate - asks a word and prints its translation");
+        System.out.println("quit - quit the text user interface");
+        System.out.println("");
+    }
+
+    public void add() {
+        System.out.print("In Finnish: ");
+        String finnishWord = read.nextLine();
+        System.out.println(finnishWord);
+        System.out.print("Translation: ");
+        String englishWord = read.nextLine();
+        System.out.println(englishWord);
+        this.dict.add(finnishWord, englishWord);
+    }
+
+    public void translate() {
+        System.out.print("Give a word: ");
+        String finnishWord = read.nextLine().trim();
+        System.out.println(finnishWord);
+        String key = dict.translate(finnishWord);
+        System.out.println("Translation: " + key);
+    }
+}
+
 class Dictionary{
     private HashMap<String, String> def;
     public Dictionary(){
@@ -20,20 +79,22 @@ class Dictionary{
         this.def.put(word,translate);
     }
     public ArrayList<String> translationList(){
-       return (this.new ArrayList<String>(this.def.values());
+        ArrayList<String> translationList = new ArrayList<String>();
+        for (String key : this.def.keySet()) {
+            String value = this.def.get(key);
+            String temp = key + " = " + value;
+            translationList.add(temp);
+        }
+        return translationList;
     }
+   
 }
 class Main {
     public static void main(String[] args) {
-    Dictionary dictionary = new Dictionary();
-    dictionary.add("apina", "monkey");
-    dictionary.add("banaani", "banana");
-    dictionary.add("cembalo", "harpsichord");
+        Scanner reader = new Scanner(System.in);
+        Dictionary dict = new Dictionary();
 
-    ArrayList<String> translations = dictionary.translationList();
-        for(String translation: translations) {
-            System.out.println(translation);
-        }
+        TextUserInterface ui = new TextUserInterface(reader, dict);
+        ui.start();
     }
 }
-  
