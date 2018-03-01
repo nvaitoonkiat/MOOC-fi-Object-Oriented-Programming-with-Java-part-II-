630f7fb6308752421b9dc8f7302a8f9ebf8d52bf
nvaitoonkiat <nvaitoonkiat@gmail.com>
2018-03-01 18:10:37 -0500

    Update Jumps.java

--- Exercise 16 Ski Jumping/src/exercise/pkg16/ski/jumping/Jumps.java
+++ Exercise 16 Ski Jumping/src/exercise/pkg16/ski/jumping/Jumps.java
@@ -5,10 +5,25 @@
  */
 package exercise.pkg16.ski.jumping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author vaitnx
  */
 public class Jumps {
+    private final List<Skiier> skiier = new ArrayList<>();
+    public static int rounds = 1;
+    public void startJump(int round){
+        System.out.println("Round " + round+1);
+        System.out.println();
+        System.out.println("Jumping order:");
+    }
+    public void printSkiiers(List<Skiier> skiiers){
+        for(Skiier skiier : this.skiier){
+                    System.out.println(skiier);
+                }
+    }
     
 }
