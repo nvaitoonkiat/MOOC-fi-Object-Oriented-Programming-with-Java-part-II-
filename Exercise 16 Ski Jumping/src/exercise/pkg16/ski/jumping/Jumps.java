2dc4af014e02e1eadcfe6314bf2c664a5fdba85b
nvaitoonkiat <nvaitoonkiat@gmail.com>
2018-03-02 17:54:04 -0500

    Update Jumps.java

--- Exercise 16 Ski Jumping/src/exercise/pkg16/ski/jumping/Jumps.java
+++ Exercise 16 Ski Jumping/src/exercise/pkg16/ski/jumping/Jumps.java
@@ -7,6 +7,7 @@ package exercise.pkg16.ski.jumping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -16,14 +17,35 @@ public class Jumps {
     private final List<Skiier> skiier = new ArrayList<>();
     public static int rounds = 1;
     public void startJump(int round){
-        System.out.println("Round " + round+1);
+        System.out.println("Round " + round);
         System.out.println();
         System.out.println("Jumping order:");
     }
     public void printSkiiers(List<Skiier> skiiers){
-        for(Skiier skiier : this.skiier){
+        for(Skiier skiier : skiiers){
                     System.out.println(skiier);
                 }
     }
-    
+    public void jump(List<Skiier> skiiers){
+        for(Skiier skiier : skiiers){
+            skiier.jump();
+        }
+    }
+    public void endRound(List<Skiier> skiiers, int round){
+        System.out.println("Results of round " + round);
+        for(Skiier skiier : skiiers){
+            skiier.printScore(round);
+        }
+    }
+    public void printFinal(List<Skiier> skiiers){
+        int count = 1;
+        System.out.println("Position           Name");
+        for(Skiier skiier : skiiers){
+            System.out.print(count + "           " + skiier.getName() + " (" + skiier.total +" points)"); 
+            count++;
+            System.out.println();
+            System.out.print("            jump lengths: " + skiier.getLength());
+            System.out.println();
+                }
+    }
 }
