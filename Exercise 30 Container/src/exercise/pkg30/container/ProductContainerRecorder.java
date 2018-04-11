6e379bf2a3a04ce3b6155e0beff47ef13d0ffb6c
nvaitoonkiat <nvaitoonkiat@gmail.com>
2018-04-11 18:58:20 -0400

    Update ProductContainerRecorder.java

--- Exercise 30 Container/src/exercise/pkg30/container/ProductContainerRecorder.java
+++ Exercise 30 Container/src/exercise/pkg30/container/ProductContainerRecorder.java
@@ -15,9 +15,40 @@ public class ProductContainerRecorder extends ProductContainer {
     public ProductContainerRecorder(String productName, double capacity, double initialVolume){
         super(productName, capacity);
         this.initialVolume = initialVolume;
+        super.addToTheContainer(this.initialVolume);
         this.history = new ContainerHistory();
+        this.history.add(this.initialVolume);
     }
     public String history(){
-        
+       return this.history.toString();
+       
     }
+    @Override
+    public void addToTheContainer(double amount) {
+        super.addToTheContainer(amount);
+        this.history.add(this.getVolume());
+    }
+      @Override
+    public double takeFromTheContainer(double amount) {
+        double firstVol = this.getVolume();
+        super.takeFromTheContainer(amount);
+        this.history.add(this.getVolume());
+        return firstVol - this.getVolume();
+    }
+
+    public void printAnalysis() {
+        System.out.println("Product: " + this.getName() + "\n"
+                + "History: " + this.history() + "\n"
+                + "Greatest product amount: " + history.maxValue() + "\n"
+                + "Smallest product amount: " + history.minValue() + "\n"
+                + "Average: " + history.average() + "\n"
+                + "Greatest change: " + history.greatestFluctuation() + "\n"
+                );
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " ProductContainerRecorder{" + "initialVolume=" + initialVolume + ", history=" + history + '}';
+    }
+    
 }
