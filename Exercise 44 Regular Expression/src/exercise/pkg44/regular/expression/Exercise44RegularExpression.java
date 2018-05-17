1b8ec7e8bd68606791bd8e57041c00dd8c39005b
nvaitoonkiat <nvaitoonkiat@gmail.com>
2018-05-17 16:54:34 -0400

    Update Exercise44RegularExpression.java

--- Exercise 44 Regular Expression/src/exercise/pkg44/regular/expression/Exercise44RegularExpression.java
+++ Exercise 44 Regular Expression/src/exercise/pkg44/regular/expression/Exercise44RegularExpression.java
@@ -3,50 +3,52 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package exercise.pkg44.regular.expression;
+package exercise.pkg44.regular.expressions;
 
 /**
  *
- * @author Neil
+ * @author vaitnx
  */
-public class Exercise44RegularExpression {
+public class Exercise44RegularExpressions {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-         String string = "tue";
-        System.out.println(isAWeekDay(string));
-        String string1 = "aeiiiiiiiiiiiii";
-        System.out.println(allVowels(string1));
-        String string2 = "99:43:10";
-        System.out.println(clockTime(string2));
-    }
-    public static boolean isAWeekDay(String string){
-        if (string.matches("mon|tue|wed|thr|fri|sat|sun")) {
-            return true;
+            // TODO code application logic here
+            // TODO code application logic here
+             String string = "tue";
+            System.out.println(isAWeekDay(string));
+            String string1 = "aeiiiiiiiiiiiii";
+            System.out.println(allVowels(string1));
+            String string2 = "19:43:10";
+            System.out.println(clockTime(string2));
         }
-        else{return false;}
-    }
-    public static boolean allVowels(String string){
-        boolean good = true;
-        for(int i = 0; i<string.length();i++){
-            char c = string.charAt(i);
-            String s = Character.toString(c);
-            if(s.matches("[a|e|i|o|u]")){
+        public static boolean isAWeekDay(String string){
+            if (string.matches("mon|tue|wed|thr|fri|sat|sun")) {
+                return true;
             }
-            else{
-                good = false;
+            else{return false;}
+        }
+        public static boolean allVowels(String string){
+            boolean good = true;
+            for(int i = 0; i<string.length();i++){
+                char c = string.charAt(i);
+                String s = Character.toString(c);
+                if(s.matches("[a|e|i|o|u]")){
+                }
+                else{
+                    good = false;
+                }
             }
+            return good;
         }
-        return good;
-    }
-    public static boolean clockTime(String string){
-        if(string.matches("[0-9]{2}:[0-9]{2}:[0-9]{2}")){
-            return true;
+        public static boolean clockTime(String string){
+            if(string.matches("[0-1]{1}[0-9]{1}:[0-6]{1}[0-9]{1}:[0-9]{2}")){
+                return true;
+            }
+            return false;
         }
-        return false;
-    }
+    
     
 }
